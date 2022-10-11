a83fa0eac02edf316782aaf548321c990e52b697
DragosBadarau <74381554+DragosBadarau@users.noreply.github.com>
2022-10-11 18:44:46 +0300

    Lab2 complet

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,36 +1,89 @@
 import numpy as np
-from scipy import stats
+from scipy.stats import expon, gamma
 from numpy import random
+import statistics
 import matplotlib.pyplot as plt
 import arviz as az
 import seaborn as sns
 
-"""
-np.random.seed(1)
 
-x = stats.norm.rvs(0, 1, size=10000) # Distributie normala cu media 0 si deviatie standard 1, 1000 samples
-y = stats.uniform.rvs(-1, 2, size=10000) # Distributie uniforma intre -1 si 1, 1000 samples . Primul parametru fiind
-# limita inferioara a intervalului, al doilea parametru fiind "marimea" intervalului, aka [-1,-1+2] = [-1,1]
-z = x+y # Compunerea prin insumare a celor 2 distributii
-
-az.plot_posterior({'x':x,'y':y,'z':z}) # Afisarea aproximarii densitatii probabilitatilor, mediei, intervalului etc.
-# variabilelor x,y,z
-plt.show()
-"""
-X1 = random.exponential(1/4,10000)
-x2 = random.exponential(1/6,10000)
+def ex1():
+    final_values = []
+    for i in range(0, 10000):
+        if random.uniform(0, 1) > 0.4:
+            final_values.append(expon.rvs(0, 0.16, 1)[0])
+        else:
+            final_values.append(expon.rvs(0, 0.25, 1)[0])
+    sns.set_style('whitegrid')
+    print(statistics.mean(final_values))
+    print(statistics.stdev(final_values))
+    sns.kdeplot(np.array(final_values), bw=0.5)
 
 
-final_values = []
+# ex1()
 
-for i in range(0,10000):
-    if random.uniform(0,1)>0.4:
-        final_values.append(x2[i])
-    else:
-        final_values.append(X1[i])
-mean_of_X1 = np.array(final_values).mean()
-sd_of_X1 = np.std(final_values)
-sns.set_style('whitegrid')
-sns.kdeplot(np.array(final_values), bw=0.5)
-#loc -> mean
-#scale -> standard deviation
+# loc -> mean
+# scale -> standard deviation
+def ex2():
+    all_processes = []
+    prob_3ms = 0
+    for i in range(1000):
+        r = random.uniform(0, 1)
+        if r < 0.25:
+            process = expon.rvs(scale=0.25, loc=0, size=1)[0] + gamma.rvs(4, 0, scale=0.33, size=1)
+            all_processes.append(process)
+        elif 0.25 <= r < 0.5:
+            process = expon.rvs(scale=0.25, loc=0, size=1)[0] + gamma.rvs(4, 0, scale=0.50, size=1)
+            all_processes.append(process)
+        elif 0.5 <= r < 0.8:
+            process = expon.rvs(scale=0.25, loc=0, size=1)[0] + gamma.rvs(5, 0, scale=0.50, size=1)
+            all_processes.append(process)
+        else:
+            process = expon.rvs(scale=0.25, loc=0, size=1)[0] + gamma.rvs(5, 0, scale=0.33, size=1)
+            all_processes.append(process)
+        if process > 3:
+            prob_3ms += 1
+    az.plot_posterior({'Density distribution of the processes': all_processes})
+    print(f'Probability that the process takes longer than 3 ms: {prob_3ms / 10}%')
+    plt.show()
+
+
+#ex2()
+
+
+def ex3():
+    bb = []
+    ss = []
+    sb = []
+    bs = []
+    experimentBB = experimentBS = experimentSB = experimentSS = 0
+    for experiment in range(1, 100):
+        for aruncari in range(1, 10):
+            masluit = random.uniform(0, 1)
+            nemasluit = random.uniform(0, 1)
+            print(masluit, nemasluit)
+            if masluit < 0.5 and nemasluit >= 0.3:
+                experimentBB += 1
+            elif masluit < 0.5 and nemasluit < 0.3:
+                experimentBS += 1
+            elif masluit >= 0.5 and nemasluit >= 0.3:
+                experimentSB += 1
+            else:
+                experimentSS += 1
+        bb.append(experimentBB)
+        bs.append(experimentBS)
+        sb.append(experimentSB)
+        ss.append(experimentSS)
+        experimentBB = 0
+        experimentSS = 0
+        experimentBS = 0
+        experimentSB = 0
+    az.plot_posterior({'ss': ss})
+    az.plot_posterior({'sb': sb})
+    az.plot_posterior({'bs': bs})
+    az.plot_posterior({'bb': bb})
+    plt.show()
+
+
+#ex3()
+
